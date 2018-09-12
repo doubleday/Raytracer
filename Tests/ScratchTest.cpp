9987ddd76b46454ecff205e0dd91067c5af70607
Daniel Doubleday <daniel.doubleday@gmx.net>
2018-09-12 08:28:10 +0200

    - initial commit

---
+++ Tests/ScratchTest.cpp
@@ -0,0 +1,22 @@
+/*
+  ==============================================================================
+
+    ScratchTest.cpp
+    Created: 12 Sep 2018 1:06:07am
+    Author:  Daniel Doubleday
+
+  ==============================================================================
+*/
+
+#include "catch.hpp"
+
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
